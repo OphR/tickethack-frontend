c5ef27c9b996be3514b52ac594b3aeb031e5ee48
Ophélie <roux.ophelie.or@gmail.com>
2024-01-30 16:56:06 +0100

    Co-authored-by: Eauksense <Eauksense@users.noreply.github.com>

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ document.querySelector("#booking").addEventListener('click', function(){
     window.location.href = 'index3.html'
 })
 
+let departure = document.querySelector(".departure").textContent
+let arrival = document.querySelector(".arrival").textContent
+let date = document.querySelector('.date').textContent
+
 document.querySelector("#button").addEventListener('click', function(){
-    fetch('http://localhost:3000/trips')
+    fetch(`http://localhost:3000/trips/${departure}/${arrival}/${date}`)
     .then(response => response.json)
     .then(data =>document.querySelector("#right-content").innerHTML = data)
 })
