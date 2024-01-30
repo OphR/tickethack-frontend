69e53a523e9bdaf5e224add8a707e12b2c433072
Ophélie <roux.ophelie.or@gmail.com>
2024-01-30 15:31:09 +0100

    FrontEnd

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector("#cart").addEventListener('click', function(){
+    window.location.href = 'index2.html'
+})
+
+document.querySelector("#booking").addEventListener('click', function(){
+    window.location.href = 'index3.html'
+})
