e5af4ebc1517e2190394fa0b073b3e1f8b7b1a68
Ophélie <roux.ophelie.or@gmail.com>
2024-01-30 16:06:39 +0100

    Co-authored-by: Eauksense <Eauksense@users.noreply.github.com>

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ document.querySelector("#cart").addEventListener('click', function(){
 document.querySelector("#booking").addEventListener('click', function(){
     window.location.href = 'index3.html'
 })
+
+document.querySelector("#button").addEventListener('click', function(){
+    fetch('http://localhost:3000/trips')
+    .then(response => response.json)
+    .then(data =>document.querySelector("#right-content").innerHTML = data)
+})
